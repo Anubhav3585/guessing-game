8fc44c239e51016105aa745a6db03b9c970f0ac0
Anubhav3585 <anubhavmahajan34@gmail.com>
2020-10-31 22:03:24 +0530

    second commit files updated

--- guessing_game.c
+++ guessing_game.c
@@ -11,50 +11,55 @@ int main()
     srand((unsigned)time(&t));
     int randomresult= rand()%21;
    //instructions to the game
-    printf("This is a guessing game::\n");
+    printf("Welcome to my Game::\n");
     printf("In this game you have to guess number between 0 to 20\n");
     printf("In this game you have only 5 tries to guess a number...........");
     printf("\n\n    GAME BEGIN    \n\n");
 
 
-    int x,y=5;
-    for (int z=1;z<=5;++z)
+    int x=0,y=5;
+
+
+    for (int z=1;z<=5;z++)
     {
-        --y;
+        y--;
         printf("\nEnter your number:: ");
         scanf("%d",&x);
         if (x>=0 && x<=20)
         {
-            if(x==randomresult)
+            if (x==randomresult)
             {
                 printf("\nyou have won the game\n\a");
                 break;
             }
 
-            else       if (x<randomresult)
+            else  {
+                      if (x<randomresult)
                       {
                           printf("\nyour number is lower\n");
-                          printf("Now you have %d tries left\a",y);
+                          printf("Now you have %d tr%s left\a",y,(y==1 || y==0)?"y":"ies");
                       }
 
-                       else
-                       {
-                           printf("\nyour number is higher\n");
-                           printf("Now you have %d tries left\a",y);
-                       }
-
+                      else
+                      {
+                          printf("\nyour number is higher\n");
+                          printf("Now you have %d tr%s left\a",y,(y==1 || y==0)?"y":"ies");
+                      }
+                  }
         }
         else
           {
-           printf("hey you must use number from 0 to 20");
-           printf("\nNow you have %d tries left\a",y);
+           printf("\nhey you must use number from 0 to 20\n");
+           printf("Now you have %d tr%s left\a",y,(y==1 || y==0)?"y":"ies");
           }
 
     }//loop ends
 
     if(x !=randomresult)
-       printf("\n\n...GAME OVER....\n\n\a");
-
+      {
+          printf("\n\n...GAME OVER....\n\a");
+          printf("The number was %d",randomresult);
+      }
 
     return 0;
 }
